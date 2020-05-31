1e77d8e3611d28fd3be0c42e352b58785b2b9323
Avinash Kumar <avinash999yadav@gmail.com>
2020-05-31 13:25:18 +0530

    final4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
+const passportLocal = require('/config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
 
